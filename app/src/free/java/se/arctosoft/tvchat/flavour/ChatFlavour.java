bd9257fd6fc304362b060bfd331799b36341cc20
Rikard Teodorsson <rikard128@pm.me>
2021-06-05 15:28:44 +0200

    Adaptive banner size

--- app/src/free/java/se/arctosoft/tvchat/flavour/ChatFlavour.java
+++ app/src/free/java/se/arctosoft/tvchat/flavour/ChatFlavour.java
@@ -1,17 +1,60 @@
 package se.arctosoft.tvchat.flavour;
 
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.widget.FrameLayout;
+
 import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
 
 import se.arctosoft.tvchat.ChatActivity;
 import se.arctosoft.tvchat.R;
 
 public class ChatFlavour {
-    private final AdView mAdView;
+    private final AdView adView;
+    private final ChatActivity activity;
 
     public ChatFlavour(ChatActivity activity) {
-        mAdView = activity.findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mAdView.loadAd(adRequest);
+        FrameLayout adContainerView = activity.findViewById(R.id.ad_view_container);
+        this.activity = activity;
+        // Step 1 - Create an AdView and set the ad unit ID on it.
+        adView = new AdView(activity);
+        adView.setAdUnitId(activity.getString(R.string.BannerMainAdId));
+        adContainerView.addView(adView);
+        loadBanner();
     }
+
+    private void loadBanner() {
+        // Create an ad request. Check your logcat output for the hashed device ID
+        // to get test ads on a physical device, e.g.,
+        // "Use AdRequest.Builder.addTestDevice("ABCDE0123") to get test ads on this
+        // device."
+        AdRequest adRequest = new AdRequest.Builder()//.addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+
+        AdSize adSize = getAdSize();
+        // Step 4 - Set the adaptive ad size on the ad view.
+        adView.setAdSize(adSize);
+
+
+        // Step 5 - Start loading the ad in the background.
+        adView.loadAd(adRequest);
+    }
+
+    private AdSize getAdSize() {
+        // Step 2 - Determine the screen width (less decorations) to use for the ad width.
+        Display display = activity.getWindowManager().getDefaultDisplay();
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        display.getMetrics(outMetrics);
+
+        float widthPixels = outMetrics.widthPixels;
+        float density = outMetrics.density;
+
+        int adWidth = (int) (widthPixels / density);
+
+        // Step 3 - Get adaptive ad size and return for setting on the ad view.
+        return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(activity, adWidth);
+    }
+
 }
